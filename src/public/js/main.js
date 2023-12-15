494daad12ad3738c8ba6d718469c59c69515edaf
Quema <myoon0929@gmail.com>
2023-12-16 00:03:04 +0900

    내용만 보내라 씨발

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,7 @@
 window.onload=()=>{
     const arrow = document.getElementById("arrow")
     const videoArrow = document.getElementById("videoArrow")
+    const menu = document.getElementById("menu")
 
     arrow.style.opacity= 0
     videoArrow.style.opacity= 0
@@ -11,35 +12,40 @@ window.onload=()=>{
                     window.location.hash = "#explanation"
                     arrow.style.opacity= null
                     arrow.style.animation= "hello 1s forwards"
-                    setTimeout(()=>{arrow.style.animation=null},1500)
+                    setTimeout(()=>{arrow.style.animation=null},1000)
                     break
             case "#video":
                     window.location.hash = "#videoDiv"
                     videoArrow.style.opacity= null
                     videoArrow.style.animation= "hello 1s forwards "
-                    setTimeout(()=>{videoArrow.style.animation=null},1500)
+                    setTimeout(()=>{videoArrow.style.animation=null},1000)
                     break
         }
-    },1000)
+
+        arrow.addEventListener("click",()=>{
+            arrow.style.animation= "opacity .5s forwards"  
+            menu.style.animation = "navup 1s forwards"    
+        })
+    
+        videoArrow.addEventListener("click",()=>{
+            videoArrow.style.animation= "opacity .5s forwards" 
+            menu.style.animation = "navdown 1s forwards"   
+            setTimeout(()=>{menu.style.animation = null},1000)
+        })
+
+    },100)
 
     switch(window.location.hash){
         case "#explanation":
                 arrow.style.opacity= null
                 arrow.style.animation= "hello 1s forwards"
-                setTimeout(()=>{arrow.style.animation=null},1500)                
+                setTimeout(()=>{arrow.style.animation=null},1000)                
                 break
         case "#videoDiv":
                 videoArrow.style.opacity= null
+                menu.style.animation = "display 0 forwards"
                 videoArrow.style.animation= "hello 1s forwards "
-                setTimeout(()=>{videoArrow.style.animation=null},1500)
+                setTimeout(()=>{videoArrow.style.animation=null},1000)
                 break
     }
-
-    arrow.addEventListener("click",()=>{
-        arrow.style.animation= "opacity .5s forwards"        
-    })
-
-    arrow.addEventListener("click",()=>{
-        videoArrow.style.animation= "opacity .5s forwards"        
-    })
 }
